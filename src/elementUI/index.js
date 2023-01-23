b9cbf5486159a72d736e951bef283acdd63e14d7
=杨洲 <1594396225@qq.com>
2023-01-23 20:07:39 +0800

    cdn引入

--- src/elementUI/index.js
+++ src/elementUI/index.js
@@ -2,8 +2,8 @@
 // 引入Vue
 import Vue from 'vue'
 // 引入并使用ElementUI
-import ElementUI from 'element-ui'
+import ELEMENT from 'element-ui'
 // 引入样式
 import 'element-ui/lib/theme-chalk/index.css'
 // 使用ElementUI
-Vue.use(ElementUI)
+Vue.use(ELEMENT)
